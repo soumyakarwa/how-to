aa6e7ff90b91a633b33fa09eabc1d8bbd4d6a51d
soumyakarwa <soumyakarwa01@gmail.com>
2023-12-01 15:06:01 -0500

    adding some visual hierarchy

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var scene1Text = ["How to write", "a book", "a story", "an article", "a grocery list", "an enemies list", "pretty much anything..."]; 
+var scene1Text = ["how to write", "a book", "a story", "an article", "a grocery list", "an enemies list", "pretty much anything..."]; 
 var scene2Text = ["Step 1:", "Scavenger Hunt", "Rummage through your house to", "find a pen that works and blank",  "sheets of lined paper!", "Or maybe find a laptop," , "if you’re not pretending" , "to be a pretentious writer."]
 var scene3Text = ["Step 2:" , "The Stare-Off", "Sit down and engage in", "an intense staring contest", "with the blank paper.", "Remember, the first one to blink loses." , "If you start to see letters dancing,", "you're doing it right."]
 var scene4Text = ["Step 3:", "Write", "Begin to write." , "It doesn't matter if your" , "first draft sounds like Shakespeare",  "or a cat walking across a keyboard.", "The key is to keep", "going until you start",  "questioning your life choices."]
@@ -6,20 +6,27 @@ var scene5Text = ["Step 4:", "Paper Toss" , "Realize that what you wrote is comp
 var scene6Text = ["Step 5:", "Back to Step 1" , "Repeat the process.", "With enough persistence, you'll either", "write the next big hit" , "or develop an impressive" , "collection of paper balls."]
 var sceneNum = 1; 
 var bodyFont; 
-var scene1Image; 
+var boldFont; 
+var scene1Image;
+var initialYPos = 20;  
+var yIncrement = 15;
+var titleIncrement = 20;   
+var titleColor = "#650D1B"; 
 
 function preload(){
-  bodyFont = loadFont("./Libre_Franklin/LibreFranklin-VariableFont_wght.ttf"); 
+  bodyFont = loadFont("./Noto_Serif/static/NotoSerif-Regular.ttf"); 
+  boldFont = loadFont("./Noto_Serif/static/NotoSerif-SemiBold.ttf"); 
   scene1Image = loadImage("./assets/img.png"); 
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont(bodyFont); 
   textAlign(LEFT, TOP); 
 }
 
 function draw() {
-  background(240, 240, 240);
+  background("#EEF4ED");
   switch (sceneNum){
     case(1):
     sceneOne(); 
@@ -43,113 +50,66 @@ function draw() {
 }
 
 function sceneOne(){
-  var initialPos = {x: width/2-textWidth(scene1Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  var imgSize = 300
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < scene1Text.length-1; i++){
-    text(scene1Text[i], width/2-textWidth(scene1Text[i])/2, initialPos.y + increment.y*i); 
-  }
+  var imgSize = 300; 
+  textSize(16); 
+  fill(titleColor); 
+  textFont(boldFont); 
+  text(scene1Text[0], width/2-textWidth(scene1Text[0])/2, initialYPos); 
+  displayText(1, scene1Text.length-1, scene1Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
   image(scene1Image, width/2-imgSize/2, height/2-imgSize/2, imgSize, imgSize); 
   text(scene1Text[scene1Text.length-1], width/2-textWidth(scene1Text[scene1Text.length-1])/2, height-20-textSize()); 
   for(let i = 1; i < scene1Text.length-1; i++){
-    lineThrough(width/2-textWidth(scene1Text[i])/2, initialPos.y + increment.y*i, scene1Text[i]); 
+    lineThrough(width/2-textWidth(scene1Text[i])/2, initialYPos + yIncrement*(i+3), scene1Text[i]); 
   }
 }
 
 function sceneTwo(){
-  var initialPos = {x: width/2-textWidth(scene2Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < 2; i++){
-    text(scene2Text[i], width/2-textWidth(scene2Text[i])/2, initialPos.y + increment.y*i); 
-  }
-  for(let i = 2; i < 5; i++){
-    text(scene2Text[i], width/2-textWidth(scene2Text[i])/2, initialPos.y + increment.y*(i+3)); 
-  }
-  for(let i = 5; i < scene2Text.length; i++){
-    text(scene2Text[i], width/2-textWidth(scene2Text[i])/2, height-3*initialPos.y + increment.y*(i-5)); 
-  }
+  displayText(0, 2, scene2Text, initialYPos, titleIncrement, 0, 16, titleColor, boldFont); 
+  displayText(2, 5, scene2Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
+  displayText(5, scene2Text.length, scene2Text, height-3*initialYPos, yIncrement, -5, 12, color(0), bodyFont); 
 }
 
 function sceneThree(){
-  var initialPos = {x: width/2-textWidth(scene3Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < 2; i++){
-    text(scene3Text[i], width/2-textWidth(scene3Text[i])/2, initialPos.y + increment.y*i); 
-  }
-  for(let i = 2; i < 5; i++){
-    text(scene3Text[i], width/2-textWidth(scene3Text[i])/2, initialPos.y + increment.y*(i+3)); 
-  }
-  for(let i = 5; i < scene3Text.length; i++){
-    text(scene3Text[i], width/2-textWidth(scene3Text[i])/2, height-3*initialPos.y + increment.y*(i-5)); 
-  }
+  displayText(0, 2, scene3Text, initialYPos, titleIncrement, 0, 16, titleColor, boldFont); 
+  displayText(2, 5, scene3Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
+  displayText(5, scene3Text.length, scene3Text, height-3*initialYPos, yIncrement, -5, 12, color(0), bodyFont); 
 }
 
 function sceneFour(){
-  var initialPos = {x: width/2-textWidth(scene4Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < 2; i++){
-    text(scene4Text[i], width/2-textWidth(scene4Text[i])/2, initialPos.y + increment.y*i); 
-  }
-  for(let i = 2; i < 6; i++){
-    text(scene4Text[i], width/2-textWidth(scene4Text[i])/2, initialPos.y + increment.y*(i+3)); 
-  }
-  for(let i = 6; i < scene4Text.length; i++){
-    text(scene4Text[i], width/2-textWidth(scene4Text[i])/2, height-3*initialPos.y + increment.y*(i-6)); 
-  }
+  displayText(0, 2, scene4Text, initialYPos, titleIncrement, 0, 16, titleColor, boldFont); 
+  displayText(2, 6, scene4Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
+  displayText(6, scene4Text.length, scene4Text, height-3*initialYPos, yIncrement, -6, 12, color(0), bodyFont); 
 }
 
-
 function sceneFive(){
-  var initialPos = {x: width/2-textWidth(scene5Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < 2; i++){
-    text(scene5Text[i], width/2-textWidth(scene5Text[i])/2, initialPos.y + increment.y*i); 
-  }
-  for(let i = 2; i < 6; i++){
-    text(scene5Text[i], width/2-textWidth(scene5Text[i])/2, initialPos.y + increment.y*(i+3)); 
-  }
-  for(let i = 6; i < scene5Text.length; i++){
-    text(scene5Text[i], width/2-textWidth(scene5Text[i])/2, height-3*initialPos.y + increment.y*(i-6)); 
-  }
+  displayText(0, 2, scene5Text, initialYPos, titleIncrement, 0, 16, titleColor, boldFont); 
+  displayText(2, 6, scene5Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
+  displayText(6, scene5Text.length, scene5Text, height-3*initialYPos, yIncrement, -6, 12, color(0), bodyFont); 
 }
 
 function sceneSix(){
-  var initialPos = {x: width/2-textWidth(scene6Text[0])/2, y: 20}; 
-  var increment = {x:10, y:15}; 
-  fill(0); 
-  textSize(12); 
-  textFont(bodyFont); 
-  for(let i = 0; i < 2; i++){
-    text(scene6Text[i], width/2-textWidth(scene6Text[i])/2, initialPos.y + increment.y*i); 
-  }
-  for(let i = 2; i < 5; i++){
-    text(scene6Text[i], width/2-textWidth(scene6Text[i])/2, initialPos.y + increment.y*(i+3)); 
-  }
-  for(let i = 5; i < scene5Text.length; i++){
-    text(scene6Text[i], width/2-textWidth(scene6Text[i])/2, height-3*initialPos.y + increment.y*(i-5)); 
-  }
+  displayText(0, 2, scene6Text, initialYPos, titleIncrement, 0, 16, titleColor, boldFont); 
+  displayText(2, 5, scene6Text, initialYPos, yIncrement, 3, 12, color(0), bodyFont); 
+  displayText(5, scene6Text.length, scene6Text, height-3*initialYPos, yIncrement, -5, 12, color(0), bodyFont); 
 }
 
 function lineThrough(x, y, txt){
   strokeWeight(0.5); 
-  line(x, y+(textSize()/20*13), x+textWidth(txt), y+(textSize()/20*13)); 
+  line(x, y+(textSize()-3), x+textWidth(txt), y+(textSize()-3)); 
 }
 
 function mousePressed(){
-  sceneNum++; 
+  sceneNum++;
+  if(sceneNum == 7){
+    sceneNum = 1; 
+  } 
+}
+
+function displayText(initialIndex, finalIndex, txt, txtYLoc, yIncrement, txtLines, fontSize, c, font){
+  fill(c); 
+  textFont(font); 
+  textSize(fontSize); 
+  for(let i = initialIndex; i < finalIndex; i++){
+    text(txt[i], width/2-textWidth(txt[i])/2, txtYLoc + yIncrement*(i + txtLines)); 
+  }
 }
